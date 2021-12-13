692a486baa1be19c245085be037da887b7b84470
Austin Engle <engleau@gmail.com>
2021-12-12 21:04:29 -0500

    inital push

---
+++ index.js
@@ -0,0 +1,53 @@
+const inquirer = require("inquirer");
+
+inquirer.prompt([
+  {
+    type: "input",
+    message: "What is your project name?",
+    name: "Projectname",
+  },
+  {
+    type: "input",
+    message: "Type a Description?",
+    name: "Description",
+  },
+  {
+    type: "input",
+    message: "Would you like a table of contents?",
+    name: "Table of Contents",
+  },
+  {
+    type: "input",
+    message: "Enter installation instructions.",
+    name: "Installation",
+  },
+  {
+    type: "input",
+    message: "Enter Usage infromation.",
+    name: "Usage",
+  },
+  {
+    type: "input",
+    message: "Who contributed to your project?",
+    name: "Contribution",
+  },
+  {
+    type: "input",
+    message: "Enter ways you tested application.",
+    name: "Test",
+  },
+]);
+
+//   Usage, License, Contributing, Tests, and Questions
+// WHEN I enter my project title
+// THEN this is displayed as the title of the README
+// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+// WHEN I choose a license for my application from a list of options
+// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+// WHEN I enter my GitHub username
+// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+// WHEN I enter my email address
+// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+// WHEN I click on the links in the Table of Contents
+// THEN I am taken to the corresponding section of the README
